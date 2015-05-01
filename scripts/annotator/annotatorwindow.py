acc3df696d1cce1acdf1970eaa5ac8f861e031cd
jheiv <jheiv@jheiv.com>
2015-05-01 17:45:44 -0400

    More cleanup, config separation

--- scripts/annotator/annotatorwindow.py
+++ scripts/annotator/annotatorwindow.py
@@ -10,6 +10,7 @@ from .common import *
 from . import config
 
 
+
 class KeySequenceState(Enum):
     WaitingInitiator = 1
     FoundInitiator = 2
@@ -21,35 +22,29 @@ class AnnotatorWindow(Gtk.Window):
         # Code completion hints, will be clobbered
         self.textbuff = Gtk.TextBuffer()
 
+        #------------------------------------------------------------------- Begin UI Initialization
         # Init
         super().__init__(title="Annotator")
         self.set_border_width(5)
         self.set_default_size(800, 850)
 
-        use_source_view = True
-        if use_source_view:
-            self.textview = GtkSource.View()
-        else:
-            self.textview = Gtk.TextView()
+        # Initialize TextView, TextBuffer, ScrolledWindow
         self.textview = GtkSource.View()
         self.textview.set_border_width(10)
-        self.textview.modify_font(Pango.font_description_from_string("Consolas 11"))
+        self.textview.modify_font(Pango.font_description_from_string(config.TEXTVIEW_FONTDESC))
         self.textview.set_editable(False)
-        #self.textview.set_size_request(800, 650)
+        self.textview.set_show_line_numbers(True)
+        gutter = self.textview.get_gutter(Gtk.TextWindowType.LEFT)
 
-        if use_source_view:
-            self.textview.set_show_line_numbers(True)
-            gutter = self.textview.get_gutter(Gtk.TextWindowType.LEFT)
+        def find_gutter_renderer():
+            for x in range(-30,30):
+                for y in range(-30,30):
+                    z = gutter.get_renderer_at_pos(x,y)
+                    if z is not None: return z
 
-            def find_gutter_renderer():
-                for x in range(-30,30):
-                    for y in range(-30,30):
-                        z = gutter.get_renderer_at_pos(x,y)
-                        if z is not None: return z
-
-            renderer = find_gutter_renderer()
-            renderer.set_background(RGBA('#363636'))
-            renderer.set_padding(7,-1)
+        renderer = find_gutter_renderer()
+        renderer.set_background(RGBA(config.TEXTVIEW_GUTTER_BG))
+        renderer.set_padding(7,-1)
 
         self.textbuff = self.textview.get_buffer()
         self.load_file(config.PEP_FILE)
@@ -58,35 +53,26 @@ class AnnotatorWindow(Gtk.Window):
         self.textswin.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)  # @UndefinedVariable
         self.textswin.add(self.textview)
 
-        info_l = ("<b>Ctrl+a, c:</b>\t Add Claim\n"
-                  "<b>Ctrl+a, e:</b>\t Add Edit\n"
-                  "<b>Ctrl+r:</b>\t\t Remove tagged selection\n")
+        # Initialize Info Labels, container Box
+        self.help_info_l = Gtk.Label(name="help_info_l", halign=Gtk.Align.START, valign=Gtk.Align.START)
+        self.help_info_l.set_markup("<b>Ctrl+a, c:</b>\t Add Claim\n"
+                                    "<b>Ctrl+a, e:</b>\t Add Edit\n"
+                                    "<b>Ctrl+r:   </b>\t Remove tagged selection\n")
 
-        info_r = ("<b>Ctrl+e, c:</b>\t Edit Claim (does nothing)\n"
-                  "<b>Ctrl+e, e:</b>\t Edit Edit\n"
-                  "<b>Ctrl+d:</b>\t\t Debug Hotkey - could do anything\n")
-
-        self.help_info_l = Gtk.Label()
-        self.help_info_l.set_markup(info_l)
-        self.help_info_l.set_halign(Gtk.Align.START)
-        self.help_info_l.set_valign(Gtk.Align.START)
-        self.help_info_l.set_name("help_info_l")
-
-        help_vsep = Gtk.VSeparator()
-
-        self.help_info_r = Gtk.Label()
-        self.help_info_r.set_markup(info_r)
-        self.help_info_r.set_halign(Gtk.Align.START)
-        self.help_info_r.set_valign(Gtk.Align.START)
-        self.help_info_r.set_name("help_info_r")
+        self.help_info_r = Gtk.Label(name="help_info_r", halign=Gtk.Align.START, valign=Gtk.Align.START)
+        self.help_info_r.set_markup("<b>Ctrl+e, c:</b>\t Edit Claim\n"
+                                    "<b>Ctrl+e, e:</b>\t Edit Edit\n"
+                                    "<b>Ctrl+d:   </b>\t Debug Hotkey - could do anything!\n")
 
         self.helpbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
         self.helpbox.pack_start(self.help_info_l, expand=True,  fill=True,  padding=5)
-        self.helpbox.pack_start(help_vsep,        expand=False, fill=False, padding=0)
+        self.helpbox.pack_start(Gtk.VSeparator(), expand=False, fill=False, padding=0)
         self.helpbox.pack_start(self.help_info_r, expand=True,  fill=True,  padding=5)
 
+        # Initialize Statusbar
         self.statusbar = Gtk.Statusbar()
 
+        # Initialize main Box
         self.mainbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.mainbox.pack_start(self.textswin,    expand=True,  fill=True,  padding=0)
         self.mainbox.pack_start(self.helpbox,     expand=False, fill=True,  padding=0)
@@ -95,65 +81,83 @@ class AnnotatorWindow(Gtk.Window):
 
         self.add(self.mainbox)
 
-        self.tagger = Tagger(self.textbuff)
-
         # CSS
         self.css_prov = Gtk.CssProvider()
-        self.css_prov.load_from_data(bytes("""
-        GtkTextView {
-            color: #EEEEEE;
-            background-color: #242424;
-        }
-
-        GtkTextView:selected {
-            color: #000000;
-            background-color: #898941;
-        }
-
-
-        """.encode()))
+        self.css_prov.load_from_data(config.APP_CSS.encode())
 
         screen = Gdk.Display.get_default().get_default_screen()
         Gtk.StyleContext.add_provider_for_screen(screen, self.css_prov, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
-        # Formatting?
-        self.tag_code = self.textbuff.create_tag("code", foreground='#CAE682')
-        self.tag_all(re.compile(r'``[^`]*``', re.MULTILINE), self.tag_code)
-        self.tag_all(re.compile(r'^.*\n\=+\n', re.MULTILINE), self.textbuff.create_tag("head1", foreground='#F0BE35'))
-        self.tag_all(re.compile(r'^.*\n\-+\n', re.MULTILINE), self.textbuff.create_tag("head2", foreground='#35BBF0'))
-        self.tag_all(re.compile(r'^.*\n\'+\n', re.MULTILINE), self.textbuff.create_tag("head3", foreground='#9F68F7'))
+        # Source Formatting?  Very basic!
+        markup_patterns = {
+            'code':  re.compile(r'``[^`]*``', re.MULTILINE),
+            'head1': re.compile(r'^.*\n\=+\n', re.MULTILINE),
+            'head2': re.compile(r'^.*\n\-+\n', re.MULTILINE),
+            'head3': re.compile(r'^.*\n\'+\n', re.MULTILINE),
+        }
 
+        for (tag_name, pattern) in markup_patterns.items():
+            style = config.MARKUP_STYLE[tag_name]
+            self.tag_all(pattern, self.textbuff.create_tag(tag_name, **style))
+
+        #--------------------------------------------------------------------- End UI Initialization
+        self.tagger = Tagger(self.textbuff)
+
+        # Key combination history
         self.key_seq_state = KeySequenceState.WaitingInitiator
         self.key_seq_hist  = None
 
         self.status_push("Initialized.")
 
 
+    # Update status bar
+    # TODO: Use this properly
     def status_push(self, text, context_desc=None):
         context_id = self.statusbar.get_context_id(str(context_desc))
         return self.statusbar.push(context_id, text)
 
 
+    # This is pretty gross, but functional
     def key_press(self, widget, event, user_data=None):
         #event = Gdk.EventKey()
         #print(event.get_state())
         #print(event.get_keycode()) # This will return the upper case version, regardless of shift
         #print(event.get_keyval())  # This will return the upper or lower case version depending
                                     #   (only on) shift (i.e. not caps lock)
+        #-------------------------------------------------------------------- Local Helper Functions
+        def handle_add_result(res):
+            if res is None:
+                self.status_push("Created tag")
+            else:
+                self.status_push(res[1])
+                self.raise_modal("Could not add tag: %s" % res[1],
+                                 message_type=Gtk.MessageType.ERROR,
+                                 buttons=Gtk.ButtonsType.OK)
+
+        def create_edit_dialog(default=''):
+            ed = EntryDialog(self,
+                             message_format="Enter edit note:",
+                             message_type=Gtk.MessageType.QUESTION,
+                             buttons=Gtk.ButtonsType.OK,
+                             default_value=default)
+            response = ed.run()
+            ed.destroy()
+            return response
+
+        #------------------------------------------------------------------------------- Begin Logic
         keyval = event.get_keyval()[1]
         keychr = chr(keyval)
         if self.key_seq_state == KeySequenceState.WaitingInitiator:
             if event.get_state() & Gdk.ModifierType.CONTROL_MASK:
-                if keychr in 'ae':
+                if keychr in 'ae':                                      # Check for initiator keys
                     self.key_seq_hist = keychr
                     self.key_seq_state = KeySequenceState.FoundInitiator
 
-                elif keychr == 'r':
-                    # Remove
+                elif keychr == 'r':                                     # Remove tag
                     self.tagger.rem_tagged_sel()
                     self.status_push("Removed tag")
 
-                elif keychr == 'd':
+                elif keychr == 'd':                                     # Debug hotkey
                     print("Ctrl+D: DEBUG")
                     print(self.textbuff.get_property('cursor-position'))
 
@@ -163,76 +167,50 @@ class AnnotatorWindow(Gtk.Window):
 
         elif self.key_seq_state == KeySequenceState.FoundInitiator:
             key_seq = (self.key_seq_hist, keychr)
-            #------------------------------------------------------- # Do something with a CLAIM tag
-            if key_seq == ('a','c'):
+            #----------------------------------------------------------------------------- Add a tag
+            if key_seq == ('a','c'):                                    # Add CLAIM tag
                 res = self.tagger.add_tagged_sel('CLAIM')
-                if res is None:
-                    self.status_push("Created tag")
-                else:
-                    self.status_push(res[1])
-                    self.raise_modal("Could not add tag: %s" % res[1],
-                                     message_type = Gtk.MessageType.ERROR,
-                                     buttons=Gtk.ButtonsType.OK)
-            elif key_seq == ('e','c'):
-                cp = self.textbuff.get_property('cursor-position')      # Get cursor position
+                handle_add_result(res)
 
+            elif key_seq == ('a','e'):                                  # Add EDIT tag
+                sel = Selection.from_buffer_selection(self.textbuff)    # Grab selection
+                note = note = create_edit_dialog()
+                res = self.tagger.add_tagged_sel('EDIT', note=note, sel=sel)
+                handle_add_result(res)
+
+            #---------------------------------------------------------------------------- Edit a tag
+            elif key_seq == ('e','c'):                                  # Edit CLAIM tag
+                cp = self.textbuff.get_property('cursor-position')      # Get cursor position
                 tag_sel_idx  = self.tagger.store.find_selection_index(cp, name_filter='CLAIM')
                 if tag_sel_idx is not None:
-                    tag_sel      = self.tagger.store[tag_sel_idx]
-                    tag_sel_note = tag_sel.note
+                    note = create_edit_dialog(self.tagger.store[tag_sel_idx].note)
+                    if note is not None:
+                        self.tagger.edit_tagged_sel(tag_sel_idx, note=note)
 
-                    ed = EntryDialog(self,
-                                     message_format="Enter edit note:",
-                                     message_type=Gtk.MessageType.QUESTION,
-                                     buttons=Gtk.ButtonsType.OK,
-                                     default_value=tag_sel_note)
-                    note = ed.run()
-                    ed.destroy()
-                    if note is not None: self.tagger.edit_tagged_sel(tag_sel_idx, note=note)
-
-            #------------------------------------------------------- # Do something with an EDIT tag
-            elif key_seq == ('a','e'):
-                sel = Selection.from_buffer_selection(self.textbuff)    # Grab selection
-                ed = EntryDialog(self,
-                                 message_format="Enter edit note:",
-                                 message_type=Gtk.MessageType.QUESTION,
-                                 buttons=Gtk.ButtonsType.OK)
-                note = ed.run()
-                ed.destroy()
-                res = self.tagger.add_tagged_sel('EDIT', note=note, sel=sel)
-
-            elif key_seq == ('e','e'):
+            elif key_seq == ('e','e'):                                  # Edit EDIT tag
                 cp = self.textbuff.get_property('cursor-position')      # Get cursor position
-
                 tag_sel_idx  = self.tagger.store.find_selection_index(cp, name_filter='EDIT')
                 if tag_sel_idx is not None:
-                    tag_sel      = self.tagger.store[tag_sel_idx]
-                    tag_sel_note = tag_sel.note
+                    note = create_edit_dialog(self.tagger.store[tag_sel_idx].note)
+                    if note is not None:
+                        self.tagger.edit_tagged_sel(tag_sel_idx, note=note)
 
-                    ed = EntryDialog(self,
-                                     message_format="Enter edit note:",
-                                     message_type=Gtk.MessageType.QUESTION,
-                                     buttons=Gtk.ButtonsType.OK,
-                                     default_value=tag_sel_note)
-                    note = ed.run()
-                    ed.destroy()
-                    if note is not None: self.tagger.edit_tagged_sel(tag_sel_idx, note=note)
-
-            # Reset key_seq stuff
+            #------------------------------------------------------------------- Reset key_seq stuff
             self.key_seq_hist = None
             self.key_seq_state = KeySequenceState.WaitingInitiator
-
 
         # Return True no matter what the event, since the TextView is not editable and to suppress
         #  the bell sounds
         return True
 
 
+    # Load a file's contents into the TextView TextBuffer
     def load_file(self, filename):
         with open(filename, "r", encoding='utf-8') as f: cont = f.read()
         self.textbuff.set_text(cont)
 
 
+    # Tag all matches of regex_obj with tag
     def tag_all(self, regex_obj, tag):
         offset = 0
         buff = self.textbuff
@@ -246,6 +224,7 @@ class AnnotatorWindow(Gtk.Window):
             buff.apply_tag(tag, siter, eiter)
 
 
+    # Prepare the window by connecting event handlers and showing it
     def prep_show(self):
         def shift_mouse_event(widget, event, user_data=None):
             wbw = widget.get_border_width()
@@ -275,11 +254,10 @@ class AnnotatorWindow(Gtk.Window):
     def on_mouse_move(self, widget, event):
         window_type = self.textview.get_window_type(event.window)
         bx, by = self.textview.window_to_buffer_coords(window_type, event.x, event.y)
-        #print(bx, by)
         (view_iter, _) = self.textview.get_iter_at_position(bx, by)
         iter_offset = view_iter.get_offset()
         tagged_sels = self.tagger.store.find_all_tagged_selections(iter_offset)
-        #print(iter_offset, len(tagged_sels))
+
         if tagged_sels:
             tooltip_text = '\n---\n'.join([ts.note for ts in tagged_sels])
             if tooltip_text.strip() == '': tooltip_text = '<i>No note specified</i>'
@@ -289,6 +267,7 @@ class AnnotatorWindow(Gtk.Window):
         self.textview.set_tooltip_markup(tooltip_text)
 
 
+    # Raise a modal notification dialog
     def raise_modal(self, message, *,
                     message_type: Gtk.MessageType=None,
                     buttons: Gtk.ButtonsType=None,
--- scripts/annotator/config.py
+++ scripts/annotator/config.py
@@ -1,9 +1,39 @@
+#===================================================================================================
+# # File Locations
+#===================================================================================================
 PEP_FILE     = '../pep/pep-3156.txt'
 PKL_TAGSTORE = 'tagstore.pkl'
 
-# The order of list entries is important here, later entries will mask earlier ones if they're
-#   "on top" of each other
-TAG_STYLE = [
-    ('CLAIM', dict(background="#314A94")),
-    ('EDIT',  dict(background="#9E2650")),
+
+#===================================================================================================
+# # UI Settings
+#===================================================================================================
+TAG_STYLE = [                                   # The order of list entries is important here,
+    ('CLAIM', dict(background="#314A94")),      #   later entries will mask earlier ones if they're
+    ('EDIT',  dict(background="#9E2650")),      #   "on top" of each other
 ]
+
+MARKUP_STYLE = {
+    'code':  dict(foreground='#CAE682'),
+    'head1': dict(foreground='#F0BE35'),
+    'head2': dict(foreground='#35BBF0'),
+    'head3': dict(foreground='#9F68F7'),
+}
+
+TEXTVIEW_FONTDESC = "Consolas 11"
+
+APP_CSS = """
+GtkTextView {
+    color: #EEEEEE;
+    background-color: #242424;
+}
+
+GtkTextView:selected {
+    color: #000000;
+    background-color: #898941;
+}
+
+
+"""
+
+TEXTVIEW_GUTTER_BG = '#363636'
